2e9ec4c397fe041683b12702d3a115b7fb77ad3f
Mohammed Salman M M <104203586+mohammed-salman-m2@users.noreply.github.com>
2025-07-31 10:13:16 +0530

    Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://scontent.fccj2-1.fna.fbcdn.net/v/t39.30808-1/475296715_644624431436694_3817992111451528103_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=103&ccb=1-7&_nc_sid=1d2534&_nc_ohc=ZhaI9ONx7IoQ7kNvwEYccjB&_nc_oc=Admxm208iGWRBvHapfrbJjfn_4TY0CA8QwKky51ybtO-UsGg18guCQodwzWmrW-O5UWlOJiircbMfx0lyO49y1MH&_nc_zt=24&_nc_ht=scontent.fccj2-1.fna&_nc_gid=SboDFCrUQUYEDRoTzKtGhQ&oh=00_AfRNACXkyDgttDq0aZQrHbUsz8vG5Utqy2rhEvEE09Zg3g&oe=6890C203" alt="Avatar" />
+          <img src="https://github.com/mohammed-salman-m2/mohammed-salman-m2.github.io/blob/master/src/assets/images/profilepic.jpg" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
